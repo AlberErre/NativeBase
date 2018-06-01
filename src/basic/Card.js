6074eefc087096d30f5a91c12ccd96774b5c81b8
akhil-geekyants <akhil@geekyants.com>
2018-06-01 20:01:45 +0530

    Attempt to pass borderRadius style's of Card to CardItem

--- src/basic/Card.js
+++ src/basic/Card.js
@@ -6,6 +6,26 @@ import { connectStyle } from "native-base-shoutem-theme";
 import mapPropsToStyleNames from "../utils/mapPropsToStyleNames";
 
 class Card extends Component {
+
+  styleChildren(style) {
+    let children = React.Children.map(this.props.children, (item, index) => {
+      let cardStyle;
+      if (index === 0) {
+        if (!item.props.style && !Array.isArray(this.props.children)) {
+          cardStyle = { borderTopLeftRadius: (style.borderTopLeftRadius || style.borderRadius), borderTopRightRadius: (style.borderTopRightRadius || style.borderRadius), borderBottomLeftRadius: (style.borderBottomLeftRadius || style.borderRadius), borderBottomRightRadius: (style.borderBottomRightRadius || style.borderRadius) }
+        } else if (!item.props.style) {
+          cardStyle = { borderTopLeftRadius: (style.borderTopLeftRadius || style.borderRadius), borderTopRightRadius: (style.borderTopRightRadius || style.borderRadius) }
+        }
+        return React.cloneElement(item, { style: cardStyle })
+      } else if (index === (this.props.children.length - 1)) {
+        cardStyle = { borderBottomLeftRadius: (style.borderBottomLeftRadius || style.borderRadius), borderBottomRightRadius: (style.borderBottomRightRadius || style.borderRadius) }
+        return React.cloneElement(item, { style: cardStyle })
+      }
+      return item;
+    })
+    return children;
+  }
+
   render() {
     if (this.props.dataArray && this.props.renderRow) {
       const ds = new ListView.DataSource({
@@ -21,9 +41,17 @@ class Card extends Component {
         />
       );
     }
+
+    let children = this.props.children;
+    let style = StyleSheet.flatten(this.props.style);
+
+    if (style && !isNaN((style.borderRadius || style.borderTopLeftRadius || style.borderTopRightRadius || style.borderBottomLeftRadius || style.borderBottomRightRadius))) {
+      children = this.styleChildren(style);
+    }
+
     return (
       <View ref={c => (this._root = c)} {...this.props}>
-        {this.props.children}
+        {children}
       </View>
     );
   }
